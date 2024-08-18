e6baf06045f5ac52b7b90f847cc645dfc6bc201b
hasitha20025 <ict20025@std.uwu.ac.lk>
2024-08-18 13:03:18 +0530

    add product pictures

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: 'Oganic Foods | Home',
+  description: '...',
+}
 
 export default function Home() {
   return (
     <>
+    <p>Home</p>
     </>
   );
 }
