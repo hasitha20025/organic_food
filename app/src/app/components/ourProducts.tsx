0d389da895647b0dd2757ef471e0880422f163dd
hasitha20025 <ict20025@std.uwu.ac.lk>
2024-08-20 14:04:47 +0530

    Update ourProducts.tsx

--- app/src/app/components/ourProducts.tsx
+++ app/src/app/components/ourProducts.tsx
@@ -5,7 +5,7 @@ export default function OurProducts() {
   return (
     <>
         <div className="container mx-auto my-8">
-            <h1 className="text-primary font-bold text-3xl text-center my-6 ">Our Products</h1>
+            <h1 className="text-primary font-bold text-2xl text-center my-6 ">Our Products</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
 
                 <ProductCard imageLink={product_01} />
