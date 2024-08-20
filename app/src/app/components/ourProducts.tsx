5338c3870048051af66793d8cde5319bf6bed12e
hasitha20025 <ict20025@std.uwu.ac.lk>
2024-08-20 14:03:47 +0530

    fix bug our products

--- app/src/app/components/ourProducts.tsx
+++ app/src/app/components/ourProducts.tsx
@@ -6,7 +6,7 @@ export default function OurProducts() {
     <>
         <div className="container mx-auto my-8">
             <h1 className="text-primary font-bold text-3xl text-center my-6 ">Our Products</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
 
                 <ProductCard imageLink={product_01} />
                 <ProductCard imageLink={product_01} />
